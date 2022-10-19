3d994b48dbe59d9091a1321be35aae7a9d28b8f4
Akash R <akashrajendran03@gmail.com>
2022-10-19 22:54:43 +0530

    pagination without package

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Header from './components/ui/Header';
 import axios from 'axios';
 import CharacterGrid from './components/characters/CharacterGrid';
 import Search from './components/ui/Search';
+import Pagination from './components/Pagination';
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(12);
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
@@ -20,11 +23,24 @@ const App = () => {
     };
     fetchItems();
   }, [query]);
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = items.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div className='container'>
       <Header />
       <Search getQuery={(q) => setQuery(q)} />
-      <CharacterGrid isLoading={isLoading} items={items} />
+      <CharacterGrid isLoading={isLoading} items={currentPosts} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={items.length}
+        paginate={paginate}
+        items={currentPosts}
+      />
     </div>
   );
 };
